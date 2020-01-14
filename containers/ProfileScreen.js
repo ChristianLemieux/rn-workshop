97d609970699eb8106b122108818fb17349b9cd0
Keith Orlando <korlando@nextjump.com>
2020-01-14 18:43:41 -0500

    add expo-constants, expo-image-picker, expo-permissions deps; add rudimentary image picking to ProfileScreen

--- containers/ProfileScreen.js
+++ containers/ProfileScreen.js
@@ -1,12 +1,54 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Image, StyleSheet, Text, View } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import Constants from 'expo-constants';
+import * as Permissions from 'expo-permissions';
+
 import { colors } from '../constants/Constants';
 
 class ProfileScreen extends Component {
-    render(){
+    state = { image: undefined };
+
+    componentDidMount() {
+        this.getPermissionsAsync();
+    }
+
+    getPermissionsAsync = async () => {
+        if (Constants.platform.ios) {
+            const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+            if (status !== 'granted') {
+                alert('Camera roll permissions are required to upload a photo!');
+            }
+        }
+    };
+
+    handleProfilePicPress = async () => {
+        const result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            allowsEditing: true,
+            aspect: [1, 1],
+            quality: 1,
+        });
+
+        console.log(result);
+
+        if (!result.cancelled) {
+            this.setState({ image: result.uri });
+        }
+    };
+
+    render() {
+        const { image } = this.state;
         return (
             <View style={styles.container}>
                 <Text style={styles.text}>My Profile Screen</Text>
+                {image && (
+                    <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />
+                )}
+                <Button
+                    title="Upload Profile Pic"
+                    onPress={this.handleProfilePicPress}
+                />
             </View>
         );
     }
