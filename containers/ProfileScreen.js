f1a136099f0023da3a6af36267e6c2021f1da9be
Keith Orlando <korlando@nextjump.com>
2020-01-15 11:59:27 -0500

    add remove pic button; better styling

--- containers/ProfileScreen.js
+++ containers/ProfileScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, Image, StyleSheet, Text, View } from 'react-native';
+import { TouchableOpacity, Image, StyleSheet, Text, View } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import Constants from 'expo-constants';
 import * as Permissions from 'expo-permissions';
@@ -39,18 +39,34 @@ class ProfileScreen extends Component {
         }
     };
 
+    handleRemovePicPress = () => {
+        this.setState({ image: undefined });
+    };
+
     render() {
         const { image } = this.state;
         return (
             <View style={styles.container}>
-                <Text style={styles.text}>My Profile Screen</Text>
                 {image && (
-                    <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />
+                    <Image source={{ uri: image }} style={styles.profileImage} />
                 )}
-                <Button
-                    title="Upload Profile Pic"
+                {!image && (
+                    <View style={styles.profileImage} />
+                )}
+                <TouchableOpacity
                     onPress={this.handleProfilePicPress}
-                />
+                    style={[styles.button, { backgroundColor: '#e9693a'}]}
+                >
+                    <Text style={styles.text}>Upload Profile Pic</Text>
+                </TouchableOpacity>
+                {image && (
+                    <TouchableOpacity
+                        onPress={this.handleRemovePicPress}
+                        style={[styles.button, { backgroundColor: '#747e95' }]}
+                    >
+                        <Text style={styles.text}>Remove Pic</Text>
+                    </TouchableOpacity>
+                )}
             </View>
         );
     }
@@ -61,14 +77,27 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: colors.mitBlue,
         color: 'white',
-        flexDirection: 'column-reverse',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
     },
     text: {
         color: 'white',
         textAlign: 'center'
-    }
+    },
+    profileImage: {
+        width: 200,
+        height: 200,
+        borderRadius: 100,
+        backgroundColor: '#86a7ff',
+        marginBottom: 32,
+    },
+    button: {
+        borderRadius: 3,
+        padding: 15,
+        width: 180,
+        marginBottom: 10,
+    },
 });
 
 export default ProfileScreen;
